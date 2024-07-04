72e757387c48972ae5c46f6a2d49bdb72dd2485a
Priscilla <priscillamikisa@gmail.com>
2024-07-04 17:19:11 +0300

    added responsiveness

--- maka-agile-app/src/Team/index.js
+++ maka-agile-app/src/Team/index.js
@@ -4,26 +4,33 @@ const Team = ()=>{
 
     return(
         <div id='team-members'>
+            <div className='threeImagesColumn'>
             <div className='image'>
-            <img src='/images/Screenshot from 2024-06-16 15-51-33.png' className='image' alt='Maureen'/>
-            <p>Senior developer</p>
+            <img src='/images/Screenshot from 2024-06-16 15-51-33.png' alt='Maureen' className='image'/>
+            <p className='mau'>Maureen Njeri</p>
             </div>
             <div className='image'>
-            <img src='/images/Screenshot from 2024-06-16 15-50-14.png' className='image' alt='Margarete'/>
-            <p>UI/UX Designer</p>
+            <img src='/images/Screenshot from 2024-06-16 15-50-14.png' alt='Margarete' className='image'/>
+            <p className='maggie'>Margarete Nyambura</p>
             </div>
             <div className='image'>
-            <img src='/images/Screenshot from 2024-06-16 15-52-27.png' className='image' alt='Susan'/>
-            <p>Android Developer</p>
+            <img src='/images/Screenshot from 2024-06-16 15-52-27.png' alt='Susan' className='image'/>
+            <p className='susan'>Susan Favour</p>
             </div>
-            <div className='image'>
-            <img src='/images/Screenshot from 2024-06-16 15-52-01.png' className='image' alt='Priscilla'/>
-            <p className='p'>ProductManager</p>
             </div>
-            <div className='image'>
-            <img src='/images/Screenshot from 2024-06-16 15-53-00.png' className='image' alt='Wato'/>
-            <p className='p' alt="Wato">Researcher</p>
+            
+            
+            <div id='twoImagesColumn'>
+            <div>
+            <img src='/images/Screenshot from 2024-06-16 15-52-01.png' alt='Priscilla' className='image'/>
+            <p className='pris'>Priscilla Nunda</p>
             </div>
+            <div>
+            <img src='/images/Screenshot from 2024-06-16 15-53-00.png' alt='Wato' className='image'/>
+            <p className="wato">Wato Bucha</p>
+            </div>
+            </div>
+            
         </div>
     )
 }
