a638dd162dcae549bc4a2cd3adc339dc08478f04
Margaret-Nyambura <margaretmacharianyambura@gmail.com>
2024-07-16 16:46:50 +0300

    made changes

--- maka-agile-app/src/About copy/index.js
+++ maka-agile-app/src/About copy/index.js
@@ -4,14 +4,14 @@ const AboutCopy = ()=>{
         <div className="container">
             <div className="about-text">
 
-            <p>We are an online learning platform for parents, teachers, and students, 
+            <p>We are an online learning platform aimed to bring together parents, teachers, and students, 
     <br></br>specifically aimed at well-resourced primary schools in Kenya. 
     <br></br>This platform will offer live classes attended by all students, led by their teachers.</p>
     
             <h3 className="head"><u>Mission</u></h3>
             <p>Our mission is to integrate communication, coordination, and community 
                 <br></br>building tools all in one easy-to-use, beautifully designed app. 
-                When it comes to <br></br>communication, Maka is meant to go beyond boundaries in bringing a sence of family.</p>
+                When it comes to <br></br>communication, Maka is meant to go beyond boundaries in bringing a sense of family.</p>
 
                 <h3 className="head"><u>Vision</u></h3>
             <p>Our vision is to go beyond boundaries and be the African communication hub that 
