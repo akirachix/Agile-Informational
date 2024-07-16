6de6d9d9212512378649c6f801ecd7252a418fe5
Margaret-Nyambura <margaretmacharianyambura@gmail.com>
2024-07-16 22:33:22 +0300

    added changes

--- maka-agile-app/src/About copy/index.js
+++ maka-agile-app/src/About copy/index.js
@@ -5,18 +5,18 @@ const AboutCopy = ()=>{
             <div className="about-text">
 
             <p>We are an online learning platform aimed to bring together parents, teachers, and students, 
-    <br></br>specifically aimed at well-resourced primary schools in Kenya. 
-    <br></br>This platform will offer live classes attended by all students, led by their teachers.</p>
+    specifically aimed at well-resourced primary schools in Kenya. 
+    This platform will offer live classes attended by all students, led by their teachers.</p>
     
             <h3 className="head"><u>Mission</u></h3>
             <p>Our mission is to integrate communication, coordination, and community 
-                <br></br>building tools all in one easy-to-use, beautifully designed app. 
-                When it comes to <br></br>communication, Maka is meant to go beyond boundaries in bringing a sense of family.</p>
+                building tools all in one easy-to-use, beautifully designed app. 
+                When it comes to communication, Maka is meant to go beyond boundaries in bringing a sense of family.</p>
 
                 <h3 className="head"><u>Vision</u></h3>
             <p>Our vision is to go beyond boundaries and be the African communication hub that 
-                <br></br>connects Schools and parents around the African globe in a very easy manner, 
-                <br></br>and facilitates communication among different cultures and personalities.</p>
+              connects Schools and parents around the African globe in a very easy manner, 
+               and facilitates communication among different cultures and personalities.</p>
 
                 </div>
               <div>
--- maka-agile-app/src/App.js
+++ maka-agile-app/src/App.js
@@ -1,30 +1,18 @@
-<<<<<<< HEAD
-import "./index.css"
-import AboutCopy from "./About copy";
-
-
-const IntroductionToAbout= ()=>{
-  return <h1 className = "heading">About Us</h1>
-};
-=======
 import Team from "./Team";
 import "./index.css"
 import Services from "./Services";
+import AboutCopy from "./About copy";
 
 
->>>>>>> aebc36e0da304a7c0ad65f34fef1706795482b0d
 
 
 const App=()=>{
   return(
     <div>
-<<<<<<< HEAD
-      <IntroductionToAbout/>
       <AboutCopy/>
-=======
             <Services/>
             <Team/>
->>>>>>> aebc36e0da304a7c0ad65f34fef1706795482b0d
+
     </div>
   )
 }
