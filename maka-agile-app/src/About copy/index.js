836a957a7522a6e20072df0441a45255192e86cf
Margaret-Nyambura <margaretmacharianyambura@gmail.com>
2024-07-17 10:27:36 +0300

    implemented the changes

--- maka-agile-app/src/About copy/index.js
+++ maka-agile-app/src/About copy/index.js
@@ -8,12 +8,12 @@ const AboutCopy = ()=>{
     specifically aimed at well-resourced primary schools in Kenya. 
     This platform will offer live classes attended by all students, led by their teachers.</p>
     
-            <h3 className="head"><u>Mission</u></h3>
+            <h3 className="mission">Mission</h3>
             <p>Our mission is to integrate communication, coordination, and community 
                 building tools all in one easy-to-use, beautifully designed app. 
                 When it comes to communication, Maka is meant to go beyond boundaries in bringing a sense of family.</p>
 
-                <h3 className="head"><u>Vision</u></h3>
+                <h3 className="vision">Vision</h3>
             <p>Our vision is to go beyond boundaries and be the African communication hub that 
               connects Schools and parents around the African globe in a very easy manner, 
                and facilitates communication among different cultures and personalities.</p>
